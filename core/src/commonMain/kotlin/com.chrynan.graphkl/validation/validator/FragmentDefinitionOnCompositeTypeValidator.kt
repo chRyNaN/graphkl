55e04665952d57b7c8b7728f5438ea28f20b92a9
Chris <ckeenan@starry.com>
2019-12-04 15:17:20 -0500

    Created FragmentDefinitionOnCompositeTypeValidator

---
+++ core/src/commonMain/kotlin/com.chrynan.graphkl/validation/validator/FragmentDefinitionOnCompositeTypeValidator.kt
@@ -0,0 +1,29 @@
+package com.chrynan.graphkl.validation.validator
+
+import com.chrynan.graphkl.language.error.GraphQLError
+import com.chrynan.graphkl.language.node.FragmentDefinitionNode
+import com.chrynan.graphkl.language.type.GraphQLCompositeType
+import com.chrynan.graphkl.validation.BaseRule
+import com.chrynan.graphkl.validation.ValidationContext
+import com.chrynan.graphkl.validation.result.Failure
+import com.chrynan.graphkl.validation.result.Success
+import com.chrynan.graphkl.validation.result.ValidationResult
+
+/**
+ * Executable definitions
+ *
+ * A GraphQL document is only valid for execution if all definitions are either
+ * operation or fragment definitions.
+ */
+class FragmentDefinitionOnCompositeTypeValidator : NodeValidator<FragmentDefinitionNode> {
+
+    override fun invoke(context: ValidationContext, node: FragmentDefinitionNode): ValidationResult {
+        val type = context.schema.namedTypeMap[node.typeCondition.name.value]
+
+        if (type != null && type !is GraphQLCompositeType) {
+            return Failure(error = GraphQLError(message = "Fragment cannot condition on non composite types. Node = $node; type = $type"))
+        }
+
+        return Success(rule = BaseRule.FRAGMENTS_ON_COMPOSITE_TYPES)
+    }
+}
