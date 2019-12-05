c23ade6f154ceed400d026f3bda33a00c3657020
Chris <ckeenan@starry.com>
2019-12-05 16:55:36 -0500

    Added additional comments to the GraphQLSchema object

--- core/src/commonMain/kotlin/com.chrynan.graphkl/language/type/schema/GraphQLSchema.kt
+++ core/src/commonMain/kotlin/com.chrynan.graphkl/language/type/schema/GraphQLSchema.kt
@@ -2,6 +2,7 @@ package com.chrynan.graphkl.language.type.schema
 
 import com.chrynan.graphkl.language.type.GraphQLNamedType
 import com.chrynan.graphkl.language.type.GraphQLObjectType
+import com.chrynan.graphkl.language.type.directive.GraphQLDirective
 import com.chrynan.graphkl.utils.reduceType
 
 /**
@@ -16,11 +17,77 @@ import com.chrynan.graphkl.utils.reduceType
  *
  * This class represents a defined Schema type that is the GraphQL API entry point. This class can then be used to
  * validate an incoming query for instance.
+ *
+ * The following is the documentation from the GraphQL Javascript Specification for the GraphQLSchema type:
+ *
+ *
+ * Schema Definition
+ *
+ * A Schema is created by supplying the root types of each type of operation,
+ * query and mutation (optional). A schema definition is then supplied to the
+ * validator and executor.
+ *
+ * Example:
+ *
+ *     const MyAppSchema = new GraphQLSchema({
+ *       query: MyAppQueryRootType,
+ *       mutation: MyAppMutationRootType,
+ *     })
+ *
+ * Note: When the schema is constructed, by default only the types that are
+ * reachable by traversing the root types are included, other types must be
+ * explicitly referenced.
+ *
+ * Example:
+ *
+ *     const characterInterface = new GraphQLInterfaceType({
+ *       name: 'Character',
+ *       ...
+ *     });
+ *
+ *     const humanType = new GraphQLObjectType({
+ *       name: 'Human',
+ *       interfaces: [characterInterface],
+ *       ...
+ *     });
+ *
+ *     const droidType = new GraphQLObjectType({
+ *       name: 'Droid',
+ *       interfaces: [characterInterface],
+ *       ...
+ *     });
+ *
+ *     const schema = new GraphQLSchema({
+ *       query: new GraphQLObjectType({
+ *         name: 'Query',
+ *         fields: {
+ *           hero: { type: characterInterface, ... },
+ *         }
+ *       }),
+ *       ...
+ *       // Since this schema references only the `Character` interface it's
+ *       // necessary to explicitly list the types that implement it if
+ *       // you want them to be included in the final schema.
+ *       types: [humanType, droidType],
+ *     })
+ *
+ * Note: If an array of `directives` are provided to GraphQLSchema, that will be
+ * the exact list of directives represented and allowed. If `directives` is not
+ * provided then a default set of the specified directives (e.g. @include and
+ * @skip) will be used. If you wish to provide *additional* directives to these
+ * specified directives, you must explicitly declare them. Example:
+ *
+ *     const MyAppSchema = new GraphQLSchema({
+ *       ...
+ *       directives: specifiedDirectives.concat([ myCustomDirective ]),
+ *     })
  */
 data class GraphQLSchema(
         val queryType: GraphQLObjectType? = null,
         val mutationType: GraphQLObjectType? = null,
-        val subscriptionType: GraphQLObjectType? = null
+        val subscriptionType: GraphQLObjectType? = null,
+        val directives: List<GraphQLDirective> = emptyList(),
+        private val types: List<GraphQLNamedType> = emptyList()
 ) {
 
     /**
