57bdeae861fc503a0ccf4b5222e66515036eda6d
Chris <ckeenan@starry.com>
2021-01-14 14:54:49 +0700

    Updated library to latest Kotlin version and consolidated library focus

---
+++ buildSrc/src/main/java/com.chrynan.graphkl.buildSrc/LibraryConstants.kt
@@ -0,0 +1,17 @@
+package com.chrynan.graphkl.buildSrc
+
+object LibraryConstants {
+
+    const val bintrayRepo = "chrynan"
+    const val group = "com.chrynan.graphkl"
+    const val owner = "chrynan"
+    const val repoName = "graphkl"
+    const val versionName = "1.0.0-SNAPSHOT"
+    const val versionCode = 1
+    const val versionDescription = "Pre-release 1.0.0-SNAPSHOT"
+    const val license = "Apache-2.0"
+    const val vcsUrl = "https://github.com/chRyNaN/graphkl.git"
+    const val androidCompileSdkVersion = 29
+    const val androidMinSdkVersion = 23
+    const val androidTargetSdkVersion = 29
+}
